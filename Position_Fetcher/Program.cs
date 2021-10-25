fb3d73bab9ad179eb39c0cdd2850580a3a7e4055
aantia <agjantia@gmail.com>
2021-10-25 16:44:55 +0100

    added timestamp and improved robustness

--- Position_Fetcher/Program.cs
+++ Position_Fetcher/Program.cs
@@ -8,7 +8,8 @@ using System.Text;
 using System.Text.Json;
 using System.Threading;
 
-namespace Position_Fetcher { 
+namespace Position_Fetcher
+{
 
     class Endpoint
     {
@@ -25,111 +26,150 @@ namespace Position_Fetcher {
 
         static void Main(string[] args)
         {
-
-            InitialiseConfig();
-
-            using (HttpClient client = new())
+            try
             {
-                client.BaseAddress = new(FTX);
 
-                while (true)
+                InitialiseConfig();
+
+                using (HttpClient client = new())
                 {
-                    FetchData(client, Endpoint.POSITION);
-                    FetchData(client, Endpoint.BALANCE);
+                    client.BaseAddress = new(FTX);
 
-                    Thread.Sleep(10 * 1000);
+                    while (true)
+                    {
+                        FetchData(client, Endpoint.POSITION);
+                        FetchData(client, Endpoint.BALANCE);
+
+                        Thread.Sleep(10 * 1000);
+                    }
                 }
-            }
 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Threw exception " + ex.StackTrace);
+                throw;
+            }
         }
 
         static void FetchData(HttpClient client, string objective)
         {
-            string endpoint = "/api/" + objective;
-            var request = new HttpRequestMessage(METHOD, endpoint);
-            HttpResponseMessage response = client.Send(Authenticate(request, endpoint));
+            try
+            {
+                string endpoint = "/api/" + objective;
+                var request = new HttpRequestMessage(METHOD, endpoint);
+                HttpResponseMessage response = client.Send(Authenticate(request, endpoint));
 
-            Console.WriteLine(response);
-            if (objective == Endpoint.POSITION)
-            {
-                ParseAndWritePositionsToCsv(response);
+                Console.WriteLine(response);
+                if (objective == Endpoint.POSITION)
+                {
+                    ParseAndWritePositionsToCsv(response);
+                }
+                else if (objective == Endpoint.BALANCE)
+                {
+                    ParseAndWriteBalancesToCsv(response);
+                }
             }
-            else if (objective == Endpoint.BALANCE)
+            catch (Exception ex)
             {
-                ParseAndWriteBalancesToCsv(response);
+                Console.WriteLine("Threw exception when fetching data: " + ex.StackTrace);
             }
         }
 
         private static void InitialiseConfig()
         {
-            using (StreamReader reader = new("config.txt"))
+            try
             {
-                for (int i = 0; i < 2; i++)
+                using (StreamReader reader = new("config.txt"))
                 {
-                    string input = reader.ReadLine();
-                    if (i == 0)
+                    for (int i = 0; i < 2; i++)
                     {
-                        YOUR_API_KEY = input.Split(' ')[1];
-                    }
-                    else if (i == 1)
-                    {
-                        YOUR_API_SECRET = input.Split(' ')[1];
+                        string input = reader.ReadLine();
+                        if (i == 0)
+                        {
+                            YOUR_API_KEY = input.Split(' ')[1];
+                        }
+                        else if (i == 1)
+                        {
+                            YOUR_API_SECRET = input.Split(' ')[1];
+                        }
                     }
                 }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Threw exception in InitialiseConfig: " + ex);
+                throw;
             }
         }
 
         public static async void ParseAndWritePositionsToCsv(HttpResponseMessage response)
         {
-            var positions = JsonSerializer.Deserialize<RootobjectPosition>(await response.Content.ReadAsStringAsync());
-
-            Console.WriteLine("\n####\n");
-            using (StreamWriter writer = new("positions.csv"))
+            try
             {
-                writer.WriteLine($"Name,Size,Side,Net Size,Long Order Size,Short Order Size,Cost,Entry Price,Unrealised PNL,Realised PNL,Initial Margin Requirement,Maintenance Margin Requirement,Open Size,Collateral Used,Estimated Liquidation Price");
-                foreach (var item in positions.result)
-                {
-                    if (item.size > 0)
-                    {
-                        string output = "";
-                        foreach (var itemB in DictionaryFromType(item))
-                        {
-                            output += itemB.Value + ",";
-                        }
-                        writer.WriteLine(output);
+                var positions = JsonSerializer.Deserialize<RootobjectPosition>(await response.Content.ReadAsStringAsync());
 
-                        Console.WriteLine(output);
+                Console.WriteLine("\n####\n");
+                using (StreamWriter writer = new("positions.csv"))
+                {
+                    writer.WriteLine($"Name,Size,Side,Net Size,Long Order Size,Short Order Size,Cost,Entry Price,Unrealised PNL,Realised PNL,Initial Margin Requirement,Maintenance Margin Requirement,Open Size,Collateral Used,Estimated Liquidation Price,Time");
+                    foreach (var item in positions.result)
+                    {
+                        if (item.size > 0)
+                        {
+                            string output = "";
+                            foreach (var itemB in DictionaryFromType(item))
+                            {
+                                output += itemB.Value + ",";
+                            }
+                            output += DateTime.Now.ToString();
+                            writer.WriteLine(output);
+
+                            Console.WriteLine(output);
+                        }
                     }
                 }
-            }
-            Console.WriteLine("\n####\n" + await response.Content.ReadAsStringAsync());
+                Console.WriteLine("\n####\n" + await response.Content.ReadAsStringAsync());
 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Threw exception in ParseAndWritePositionsToCsv: " + ex.StackTrace);
+            }
         }
 
         public static async void ParseAndWriteBalancesToCsv(HttpResponseMessage response)
         {
-            var balances = JsonSerializer.Deserialize<RootobjectBalance>(await response.Content.ReadAsStringAsync());
-
-            Console.WriteLine("\n####\n");
-            using (StreamWriter writer = new("balances.csv"))
+            try
             {
-                writer.WriteLine($"Name,Free,Spot Borrow,Total,USD Value,Available Without Borrow");
-                foreach (var item in balances.result)
-                {
-                    if (item.total > 0)
-                    {
-                        string output = "";
-                        foreach (var itemB in DictionaryFromType(item))
-                        {
-                            output += itemB.Value + ",";
-                        }
-                        writer.WriteLine(output);
 
-                        Console.WriteLine(output);
+                var balances = JsonSerializer.Deserialize<RootobjectBalance>(await response.Content.ReadAsStringAsync());
+
+                Console.WriteLine("\n####\n");
+                using (StreamWriter writer = new("balances.csv"))
+                {
+                    writer.WriteLine($"Name,Free,Spot Borrow,Total,USD Value,Available Without Borrow");
+                    foreach (var item in balances.result)
+                    {
+                        if (item.total > 0)
+                        {
+                            string output = "";
+                            foreach (var itemB in DictionaryFromType(item))
+                            {
+                                output += itemB.Value + ",";
+                            }
+                            writer.WriteLine(output);
+
+                            Console.WriteLine(output);
+                        }
                     }
                 }
+                Console.WriteLine("\n####\n" + await response.Content.ReadAsStringAsync());
             }
-            Console.WriteLine("\n####\n" + await response.Content.ReadAsStringAsync());
+            catch (Exception ex)
+            {
+                Console.WriteLine("Threw exception in ParseAndWriteBalancesToCsv: " + ex.StackTrace);
+            }
 
         }
 
@@ -149,19 +189,27 @@ namespace Position_Fetcher {
 
         static HttpRequestMessage Authenticate(HttpRequestMessage request, string endpoint)
         {
-            var _nonce = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            try
+            {
+                var _nonce = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
 
-            var hashMaker = new HMACSHA256(Encoding.UTF8.GetBytes($"{YOUR_API_SECRET}"));
-            var signaturePayload = $"{_nonce}{METHOD.ToString().ToUpper()}{endpoint}";
-            var hash = hashMaker.ComputeHash(Encoding.UTF8.GetBytes(signaturePayload));
-            var hashString = BitConverter.ToString(hash).Replace("-", string.Empty);
-            var signature = hashString.ToLower();
+                var hashMaker = new HMACSHA256(Encoding.UTF8.GetBytes($"{YOUR_API_SECRET}"));
+                var signaturePayload = $"{_nonce}{METHOD.ToString().ToUpper()}{endpoint}";
+                var hash = hashMaker.ComputeHash(Encoding.UTF8.GetBytes(signaturePayload));
+                var hashString = BitConverter.ToString(hash).Replace("-", string.Empty);
+                var signature = hashString.ToLower();
 
-            request.Headers.Add("FTX-KEY", YOUR_API_KEY);
-            request.Headers.Add("FTX-SIGN", signature);
-            request.Headers.Add("FTX-TS", _nonce.ToString());
+                request.Headers.Add("FTX-KEY", YOUR_API_KEY);
+                request.Headers.Add("FTX-SIGN", signature);
+                request.Headers.Add("FTX-TS", _nonce.ToString());
 
-            return request;
+                return request;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Threw exception in Authenticate: " + ex);
+                throw;
+            }
         }
     }
 }
